b08a966b324e14443b9aad3388a0d79f0213fed0
vabbu82 <vabbu82@gmail.com>
2016-10-03 21:32:55 -0400

    example2 commit

---
+++ src/example1/UserDetails.java
@@ -0,0 +1,21 @@
+package example1;
+
+import java.util.Scanner;
+
+public class UserDetails {
+	public static void main(String[] args) {
+		Scanner input = new Scanner(System.in);
+		System.out.println("Please enter User Details Name ");
+		System.out.println("Please enter User Name");
+		String name = input.nextLine();
+		System.out.println("Please enter User Email Id ");
+		String emailId = input.nextLine();
+		System.out.println("Please enter User Phone ");
+		int phone = input.nextInt();
+		
+		System.out.println("User Details Entered ::");
+		System.out.println("User Name :: " + name);
+		System.out.println("User Phone :: "+phone);
+		System.out.println("User Email :: "+ emailId );
+	}
+}
