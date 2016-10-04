252a6fbd434925819228617ad1d058e78c236837
vabbu82 <vabbu82@gmail.com>
2016-10-03 20:45:14 -0400

    format corrected

--- src/example1/helloworld.java
+++ src/example1/helloworld.java
@@ -1,7 +1,7 @@
 package example1;
 
 public class helloworld {
-public static void main(String[] args) {
-	System.out.println("Hello World");
-}
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
 }
