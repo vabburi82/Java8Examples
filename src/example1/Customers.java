89303b65debeabeb1128e618048909d598f9fd57
anil a <aakumar2010@gmail.com>
2016-10-07 11:53:34 -0400

    by entering no of customers, it will ask the customer details.

---
+++ src/example1/Customers.java
@@ -0,0 +1,33 @@
+package example1;
+import java.util.Scanner;
+public class Customers
+{
+public static void main(String[] args) 
+	{
+    int i, j;
+    @SuppressWarnings("resource")
+	Scanner input = new Scanner(System.in);
+    System.out.print("\nEnter No. of customers : ");
+    j = input.nextInt();
+    
+    for (i = 0; i < j; i++) 
+    	{
+        System.out.println("\nEnter Customer [ " + (i + 1) + " ] details ::");
+        
+        System.out.println("Enter the name: ");
+        String Name = input.nextLine();
+        
+        System.out.println("Enter the Email: ");
+        String Email = input.nextLine();
+        
+        System.out.println("Enter the PhoneNo: ");
+        String phone = input.nextLine();
+        
+        System.out.println("User Details Entered are ::\n");
+		System.out.println("User Name :: "+ Name);
+		System.out.println("User Phone :: "+ phone);
+		System.out.println("User Email :: "+ Email);
+    	}
+   
+	}
+}
