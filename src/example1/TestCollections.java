da587f40ec2cb79888fde2c0d7517002fa22623f
anil a <aakumar2010@gmail.com>
2016-10-10 12:43:13 -0400

    CustomerDetails using Collections

--- src/example1/TestCollections.java
+++ src/example1/TestCollections.java
@@ -16,83 +16,53 @@ public static void main(String[] args) throws IOException
 			  }
 			 	BufferedWriter bw =new BufferedWriter(new FileWriter(file,true));
 			 	PrintWriter pw = new PrintWriter(bw);
-			    int i;
-				String name;
-				String email;
-				String phone;
-				Scanner input =new Scanner(System.in);
+			 	ArrayList<String> l = new ArrayList<String>();
+			 	Scanner input =new Scanner(System.in);
 				System.out.println("Welcome to Costumer Deatails page");
-				
-			   // Customer names[]=new Customer[1000];
-				List<String> =toList(String[] array) {
-
-				    List<String> list = new ArrayList(array.length);
-
-				    list.addAll(Arrays.asList(array));
-
-				    return list;
-				}
-
-			    System.out.println("Are you creating a record:");
+				System.out.println("Are you creating a record:");
 				String userReply=input.next();
-				
-				for (i = 0; "Y".equalsIgnoreCase(userReply); i++)
+				for (i = 1; "Y".equalsIgnoreCase(userReply); i++)
 		    		{
 		    	
 				        System.out.println("\nEnter Customer"+ "[" + i + "]"+ "details ::");
 				        
 				        System.out.print("Enter the name: ");
 				        name = input.next();
-				        
+				        l.add(name);
 				        System.out.print("Enter the Email: ");
 				        email = input.next();
-				        
+				        l.add(email);
 				        System.out.print("Enter the PhoneNo: ");
 				        phone = input.next();
+				        l.add(phone);
 				        
 						System.out.println("\nDo you want to  enter more customers : ");
-						userReply=input.next();
+						userReply=input.next();	
 						
-						names[i]=new Customer();
-						names[i].name=name;
-						names[i].email=email;
-						names[i].phone=phone;	
 		    		}
-				
-							 System.out.println("User Details Entered are ::\n");
-							 pw.println("****************************");
+				             pw.println("****************************");
 							 pw.println(d.toString());
-							 for(int j=0;j<i;j++)
-				 	{	 
-					 System.out.println(d.toString());
-					 System.out.println("Customer" + j+ "Details: ");
-					 System.out.println("Name ::" + names[j].name);
-					 System.out.println("Email :: "+ names[j].email);
-				     System.out.println("Phone :: "+ names[j].phone);
-				     System.out.println("================================");
-				     pw.println("Customer" +"["+ j+"]"+ "Details: ");
-				     pw.println("Name:"+ names[j].name);
-					 pw.println("Email:"+names[j].email);
-					 pw.println("Phone:"+names[j].phone);
-					 pw.println("================================");
-					 
-				 	}
+							 
+				      for(int k=0;k<l.size();k++)
+				      { 
+				       pw.println("Customer" +"["+f+"]"+ "Details: ");
+		               pw.println("Name:"+ l.get(k));
+					   k++;
+					   pw.println("Email:"+l.get(k));
+					   k++;
+					   pw.println("Phone:"+l.get(k));
+					   pw.println("================================");
+					   f= f+g;
+					   f++;
+					 }
 				 pw.close();
-				 file = new File("C:/myfile.txt");
-		 
 		         FileReader fr = new FileReader(file);
 		         BufferedReader br = new BufferedReader(fr);
 		         String line=br.readLine();
 		         while(line!=null)
 		         {
-		        
 		         System.out.println(line);
 		         line=br.readLine();
-		         }
+                 }
       }
-
-private static List<String> toList(Object object) {
-	// TODO Auto-generated method stub
-	return null;
-}
 }
