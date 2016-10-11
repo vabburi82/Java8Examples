4a9117423820adf559db553085c34b3bbb6110ef
anil a <aakumar2010@gmail.com>
2016-10-11 12:09:29 -0400

    Customer list for user records by using setter and getter methods

---
+++ src/example1/CustomerList.java
@@ -0,0 +1,39 @@
+package example1;
+public class CustomerList {
+	private String name;
+	private String email;
+	private int phone;
+	private int age;
+	private float height;
+	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public int getPhone() {
+		return phone;
+	}
+	public void setPhone(int phone) {
+		this.phone = phone;
+	}
+	public int getAge() {
+		return age;
+	}
+	public void setAge(int age) {
+		this.age = age;
+	}
+	public float getHeight() {
+		return height;
+	}
+	public void setHeight(float height) {
+		this.height = height;
+	}
+}
